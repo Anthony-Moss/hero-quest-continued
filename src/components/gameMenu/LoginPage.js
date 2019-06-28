cfe92fac4fec2afe7a70b2f09dcb7e6f8d1b893b
Anthony Moss <asmoss93@gmail.com>
2019-06-27 22:36:00 -0400

    adds form to login page and basic styling, functionality not completed

--- src/components/gameMenu/LoginPage.js
+++ src/components/gameMenu/LoginPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import qs from 'qs'
 import gameMenuBackground from '../../styles/backgroundImages/AH-Space copy.png'
-import './gameMenu.css'
+import './loginPage.css'
 
 class LoginPage extends React.Component {
     constructor(props) {
@@ -13,6 +13,8 @@ class LoginPage extends React.Component {
         //     messages: [],
         //     text: ''
         // }
+        this.state = {}
+        this.handleTextInputChange = this.handleTextInputChange.bind(this);
     }
 
     // async componentDidMount() {
@@ -38,16 +40,35 @@ class LoginPage extends React.Component {
                     width: '640px',
                     height: '640px',
                     margin: '20px auto'}}>
-                <div className='TitleText'>
+                <div className='TitleTextLP'>
                     <h1>Hero Quest</h1>
+                    <h2>Enter your information below to login and play!</h2>
+                </div>
+                <div className='loginPageForm'>
+                    <form onSubmit={this.userLogin}>
+                        <label>
+                            Email:
+                            <input name="email" type="text" value={this.state.email} onChange={this.handleTextInputChange} />
+                        </label>
+                        <label>
+                            Password:
+                            <input name="password" type="password" value={this.state.password} onChange={this.handleTextInputChange} />
+                        </label>
+                            <input type="submit" value="Login" />
+                    </form>
                 </div>
                 <div className='LoginScreenButtons'>
                     {/* this will take the input fields info and test login, if successful goes to main gameMenu to load or start new */}
-                    <button onClick={this.userLogin}>Login</button>
+                    <button onClick={this.toGameMenuFromLogin}>Main Menu</button>
                     <button onClick={this.createUserPage}>Create Account</button>
                 </div>
             </div>
         )
+    }
+
+    // this handles updating the text input of the createUser text inputs
+    handleTextInputChange = (event) => {
+        this.setState({[event.target.name]: event.target.value});
     }
     // this will call functions needed to dispatch actions associated with logging in
     // will make sure user is logging in (has correct username pw, user existss etc)
